3983a3f9835e45f49f7718dc22d1739dda4fa9a1
hoops92 <hoops92@live.com>
2020-06-17 09:39:55 -0600

    added

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    mid = (start+end)//2
+    if start > end:
+        return -1
+    else:
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            return binary_search(arr, target, mid+1, end)
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid-1)
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
+
+arr = [1,2,3,4,5,6,7,8,9]
+
+test = binary_search(arr, 9, 0, len(arr)-1)
+print(test)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,30 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-
+# TO-DO: Implement a recursive implementation of binary search
+def binary_search(arr, target, start, end):
     # Your code here
+    mid = (start+end)//2
+    if start > end:
+        return -1
+    else:
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            return binary_search(arr, target, mid+1, end)
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid-1)
 
 
-    return merged_arr
-
-# TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
+# STRETCH: implement an order-agnostic binary search
+# This version of binary search should correctly find
+# the target regardless of whether the input array is
+# sorted in ascending order or in descending order
+# You can implement this function either recursively
+# or iteratively
+def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
 
-    return arr
+arr = [1,2,3,4,5,6,7,8,9]
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+test = binary_search(arr, 9, 0, len(arr)-1)
+print(test)
